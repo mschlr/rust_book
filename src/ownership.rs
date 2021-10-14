c5610e783de3fb731077a292dbed4ea1f62a1be1
Martin Schuler <martin.schuler@outlook.com>
2021-10-14 18:06:23 +0200

    added more Strings and UTF-8 character examples

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,6 +1,31 @@
 pub fn hello() {
+    // Book Example
     let my_string = String::from("Flughafen Zürich (ZRH)");
     println!("{}", first_word(&my_string));
+
+    // Some more String and UTF-8 Examples
+    let mut hello = String::from("Hello");
+
+    // Get string length
+    println!("Get the length of '{}': {}", hello, hello.len());
+
+    hello.push(' '); // `empty` char
+    hello.push('W');
+    hello.push_str("orld.");
+
+    print!("{}", hello);
+
+    // String capacity
+    println!(": has a capacity of {}", hello.capacity());
+
+    // From Rust Book
+    let strastwujtie = "Здравствуйте";
+    let sawadeekrap = "สวัสดีครับ";
+    let bind = format!("Thai: {}\nRussian: {}", sawadeekrap, strastwujtie);
+
+    println!("\n{}\n", bind);
+    println!("{}", &strastwujtie[0..4]);
+    println!("{}", &sawadeekrap[0..6]);
 }
 
 fn first_word(s: &str) -> &str {
